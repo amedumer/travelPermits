b6f1931c2c64140200a2c9f8a563141d48cedbc3
Ahmet Ömer Kayabaşı <amet@Ahmet-MacBook-Pro.local>
2021-02-12 11:50:40 +0300

    Ropsten Support v1

---
+++ docs/precache-manifest.fb3f8eaf59ab2badadc1f28bb795e0b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "28b2669937043f66310a",
+    "url": "/static/css/main.eea93f4e.chunk.css"
+  },
+  {
+    "revision": "28b2669937043f66310a",
+    "url": "/static/js/main.28b26699.chunk.js"
+  },
+  {
+    "revision": "2f869a54e2d7b97dff03",
+    "url": "/static/css/1.66c17a6f.chunk.css"
+  },
+  {
+    "revision": "2f869a54e2d7b97dff03",
+    "url": "/static/js/1.2f869a54.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "2ceaf5634979b8d68ac38dfa69bd7c17",
+    "url": "/index.html"
+  }
+];
